86210fbb0953b0a45b4d36db404872cec178ecb9
Mario <mariob360@gmail.com>
2021-03-23 19:07:26 -0700

    create about us and subcribe page

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const about = props => {
+  console.log(props);
+  return (
+    <div>
+      <p>About us Page</p>
+    </div>
+  );
+};
+
+export default about;
---
+++ pages/subscribe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const subscribe = props => {
+  console.log(props);
+  return (
+    <div>
+      <p>Subcribe page</p>
+    </div>
+  );
+};
+
+export default subscribe;
