8477b8810b18ca7427cef642e1908e6c3eeec167
Mario <mariob360@gmail.com>
2021-03-25 14:55:40 -0700

    create all the data for store coffees in array This will be stored in CollectionData.js

---
+++ components/collection/CollectionData.js
@@ -0,0 +1,39 @@
+// 454 x 356
+export const collectionData = [
+  {
+    width: 234,
+    height: 177,
+    src: '/images/coffee/image-gran-espresso.png',
+    alt: 'Gran Espresso Coffee',
+    name: 'Gran Espresso',
+    desc:
+      'Light and flavorful blend with cocoa and black pepper for an intense experience.',
+  },
+  {
+    width: 234,
+    height: 177,
+    src: '/images/coffee/image-planalto.png',
+    alt: 'Planalto Coffee',
+    name: 'Planalto',
+    desc:
+      'Brazilian dark roast with rich and velvety body, and hints of fruits and nuts.',
+  },
+  {
+    width: 234,
+    height: 177,
+    src: '/images/coffee/image-Piccollo.png',
+    alt: 'Piccollo Coffee',
+    name: 'Piccollo',
+    desc:
+      'Mild and smooth blend featuring notes of toasted almond and dried cherry.',
+  },
+  {
+    width: 234,
+    height: 177,
+    src: '/images/coffee/image-danche.png',
+    alt: 'Danche Coffee',
+    name: 'Danche',
+    desc:
+      'Ethiopian hand-harvested blend densely packed with vibrant fruit notes.',
+  },
+];
