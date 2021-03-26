92d7be1a333e1742a888c7b2adeced9d76228d38
Mario <mariob360@gmail.com>
2021-03-25 18:14:53 -0700

    Add in "our collection" text

--- components/collection/Collection.js
+++ components/collection/Collection.js
@@ -5,24 +5,27 @@ import { collectionData } from './CollectionData';
 
 const Collection = () => (
   <CollectionStyles>
-    {/* TODO: BACKGROUND WITH "OUR COLLECTION" */}
-    {collectionData.map((item, i) => (
-      <div className="collection-item" key={i}>
-        <Image
-          className="collection-image"
-          src={item.src}
-          width={item.width}
-          height={item.height}
-          quality={100}
-          object-fit="contain"
-          alt={item.alt}
-        />
-        <div className="collection-text">
-          <h4>{item.name}</h4>
-          <p>{item.desc}</p>
+    <div className="collection-lg-title">Our Collection</div>
+
+    <div className="collection-items">
+      {collectionData.map((item, i) => (
+        <div className="collection-item" key={i}>
+          <Image
+            className="collection-image"
+            src={item.src}
+            width={item.width}
+            height={item.height}
+            quality={100}
+            object-fit="contain"
+            alt={item.alt}
+          />
+          <div className="collection-text">
+            <h4>{item.name}</h4>
+            <p>{item.desc}</p>
+          </div>
         </div>
-      </div>
-    ))}
+      ))}
+    </div>
   </CollectionStyles>
 );
 
--- components/collection/CollectionStyles.js
+++ components/collection/CollectionStyles.js
@@ -2,27 +2,53 @@ import styled from 'styled-components';
 
 const CollectionStyles = styled.div`
   display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 12.5rem;
 
   /* FOR FUTURE TEXT */
-  /* flex: 0 0 100%;  */
-  .collection-item {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    img {
-    }
+  .collection-lg-title {
+    font-family: 'Fraunces', serif;
+    text-align: center;
+    text-transform: lowercase;
+    font-size: 9.375rem;
+    line-height: normal;
 
-    .collection-text {
-      margin-top: 4.5rem;
-      width: 75%;
-      text-align: center;
-      h4 {
-        margin-bottom: 1em;
-      }
-      p {
-        color: var(--darkGreyBlue);
-        max-height: 78px;
+    background: linear-gradient(180deg, var(--grey) 0%, #ffffff 80%);
+    background-size: 100%;
+
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+  }
+  .collection-items {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    padding: 0 5.4em;
+    margin-top: -75px;
+
+    .collection-item {
+      display: flex;
+      flex: 0 0 256px;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      margin-bottom: 2rem;
+
+      .collection-text {
+        margin-top: 4.5rem;
+        width: 85%;
+        text-align: center;
+        h4 {
+          margin-bottom: 1em;
+        }
+        p {
+          color: var(--darkGreyBlue);
+          max-height: 78px;
+        }
       }
     }
   }
