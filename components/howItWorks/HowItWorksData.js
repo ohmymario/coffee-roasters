ff06efab7c04789ed42c7222baf7dd98a93ae196
Mario <mariob360@gmail.com>
2021-04-01 17:16:54 -0700

    create data needed for HowItWorks component

---
+++ components/howItWorks/HowItWorksData.js
@@ -0,0 +1,17 @@
+export const HowItWorksData = [
+  {
+    title: 'Pick your coffee',
+    subtext:
+      'Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices for them. There are new coffees in all profiles every month for you to try out.',
+  },
+  {
+    title: 'Choose the frequency',
+    subtext:
+      'Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.',
+  },
+  {
+    title: 'Receive and enjoy!',
+    subtext:
+      'We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.',
+  },
+];
