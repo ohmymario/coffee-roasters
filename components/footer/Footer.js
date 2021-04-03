376860ba3e8af4022227b255ba227b27b7095139
Mario <mariob360@gmail.com>
2021-04-02 19:06:25 -0700

    create footer component files

---
+++ components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  console.log('footer');
+  return (
+    <div>
+      <p>Here goes the footer</p>
+    </div>
+  );
+};
+
+export default Footer;
