27e9f83ce7f4e6ba70221ac6376ad4dd459ec3c4
Mario <mariob360@gmail.com>
2021-04-05 16:04:08 -0700

    add all routes to routes.js file

---
+++ constants/routes.js
@@ -0,0 +1,14 @@
+export const routes = [
+  {
+    name: 'home',
+    route: '/',
+  },
+  {
+    name: 'about Us',
+    route: '/about',
+  },
+  {
+    name: 'create your plan ',
+    route: '/subscribe',
+  },
+];
