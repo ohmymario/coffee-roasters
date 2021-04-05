fa3722fb41772fe2f2f9a56ec3da69dac0b9aa51
Mario <mariob360@gmail.com>
2021-04-05 14:57:22 -0700

    Create footer component with desktop view CSS

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,11 +1,67 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import FooterStyles from './FooterStyles';
 
 const Footer = () => {
   console.log('footer');
   return (
-    <div>
-      <p>Here goes the footer</p>
-    </div>
+    <FooterStyles>
+      <div className="footer">
+        <div className="footer-logo">
+          <Image
+            src="/images/logo-footer.svg"
+            layout="fixed"
+            alt="logo"
+            width={235}
+            height={25}
+          />
+        </div>
+
+        <ul className="footer-links">
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About Us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/subscribe">
+              <a>Create your plan</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="socials">
+        <Image
+          src="/images/socials/icon-facebook.svg"
+          layout="fixed"
+          alt="logo"
+          width={24}
+          height={24}
+        />
+        <Image
+          src="/images/socials/icon-twitter.svg"
+          layout="fixed"
+          alt="logo"
+          width={24}
+          height={24}
+        />
+        <Image
+          src="/images/socials/icon-instagram.svg"
+          layout="fixed"
+          alt="logo"
+          width={24}
+          height={24}
+        />
+      </div>
+    </FooterStyles>
   );
 };
 
--- components/footer/FooterStyles.js
+++ components/footer/FooterStyles.js
@@ -1,1 +1,40 @@
 import styled from 'styled-components';
+
+const FooterStyles = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 120px;
+  background: var(--darkGreyBlue);
+  padding: 0 85px 0 85px;
+  .footer {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 620px;
+    flex: 1 1 auto;
+
+    &-links {
+      display: flex;
+      li {
+        text-transform: uppercase;
+        font-weight: 700;
+        font-size: 0.75rem;
+        letter-spacing: 0.92px;
+        &:not(:last-child) {
+          padding-right: 2.85em;
+        }
+      }
+    }
+  }
+
+  .socials {
+    div {
+      &:not(:last-child) {
+        margin-right: 1.5em;
+      }
+    }
+  }
+`;
+
+export default FooterStyles;
