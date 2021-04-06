da041fff47e0bbba64ef3f0ecb79d0cd08a7c5c2
Mario <mariob360@gmail.com>
2021-04-06 15:48:21 -0700

    remove old console logs

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -5,49 +5,46 @@ import FooterStyles from './FooterStyles';
 import PageLinks from '../pageLinks/PageLinks';
 import { routes } from '../../constants/routes';
 
-const Footer = () => {
-  console.log('footer');
-  return (
-    <FooterStyles>
-      <div className="footer">
-        <div className="footer-logo">
-          <Image
-            src="/images/logo-footer.svg"
-            layout="fixed"
-            alt="logo"
-            width={235}
-            height={25}
-          />
-        </div>
-
-        <PageLinks routes={routes} />
-      </div>
-
-      <div className="socials">
+const Footer = () => (
+  <FooterStyles>
+    <div className="footer">
+      <div className="footer-logo">
         <Image
-          src="/images/socials/icon-facebook.svg"
+          src="/images/logo-footer.svg"
           layout="fixed"
           alt="logo"
-          width={24}
-          height={24}
-        />
-        <Image
-          src="/images/socials/icon-twitter.svg"
-          layout="fixed"
-          alt="logo"
-          width={24}
-          height={24}
-        />
-        <Image
-          src="/images/socials/icon-instagram.svg"
-          layout="fixed"
-          alt="logo"
-          width={24}
-          height={24}
+          width={235}
+          height={25}
         />
       </div>
-    </FooterStyles>
-  );
-};
+
+      <PageLinks routes={routes} />
+    </div>
+
+    <div className="socials">
+      <Image
+        src="/images/socials/icon-facebook.svg"
+        layout="fixed"
+        alt="logo"
+        width={24}
+        height={24}
+      />
+      <Image
+        src="/images/socials/icon-twitter.svg"
+        layout="fixed"
+        alt="logo"
+        width={24}
+        height={24}
+      />
+      <Image
+        src="/images/socials/icon-instagram.svg"
+        layout="fixed"
+        alt="logo"
+        width={24}
+        height={24}
+      />
+    </div>
+  </FooterStyles>
+);
 
 export default Footer;
--- pages/about.js
+++ pages/about.js
@@ -2,20 +2,17 @@ import React from 'react';
 import Head from 'next/head';
 import AboutUs from '../components/aboutUs/AboutUs';
 
-const about = props => {
-  console.log(props);
-  return (
-    <>
-      <Head>
-        <title>coffeeroasters</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const about = props => (
+  <>
+    <Head>
+      <title>coffeeroasters</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-      <main>
-        <AboutUs />
-      </main>
-    </>
-  );
-};
+    <main>
+      <AboutUs />
+    </main>
+  </>
+);
 
 export default about;
