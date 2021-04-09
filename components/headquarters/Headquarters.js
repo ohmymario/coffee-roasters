e3c584e4d7413976eb95bf16a6e0320e30a4d96b
Mario <mariob360@gmail.com>
2021-04-09 10:29:30 -0700

    create files for headquarters component

---
+++ components/headquarters/Headquarters.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Headquarters = () => {
+  console.log('headquarters component');
+  return (
+    <div>
+      <p>headquarters component</p>
+    </div>
+  );
+};
+
+export default Headquarters;
