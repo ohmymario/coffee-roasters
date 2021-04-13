5420c3e231ba768f073cc245d24f050e354aee87
Mario <mariob360@gmail.com>
2021-04-12 17:12:24 -0700

    create data for headquarters component

--- components/headquarters/Headquarters.js
+++ components/headquarters/Headquarters.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import { headquartersData } from './HeadquartersData';
+
+// example input
+// country: 'United Kingdom',
+// phone: '+44 1241 918425',
+// address: `68 Asfordby Rd
+// Alcaston
+// SY61YA`,
+// img: '/images/headquarters/illustration-uk.svg',
 
 const Headquarters = () => {
   console.log('headquarters component');
---
+++ components/headquarters/HeadquartersData.js
@@ -0,0 +1,24 @@
+export const headquartersData = [
+  {
+    country: 'United Kingdom',
+    phone: '+44 1241 918425',
+    address: `68 Asfordby Rd
+    Alcaston
+    SY61YA`,
+    img: '/images/headquarters/illustration-uk.svg',
+  },
+  {
+    country: 'Canada',
+    phone: '+1 416 485 2997',
+    address: `1528 Eglinton Avenue
+    Toronto
+    Ontario M4P1A6`,
+    img: '/images/headquarters/illustration-canada.svg',
+  },
+  {
+    country: 'Australia',
+    phone: '+61 4 9928 3629',
+    address: '/images/coffee/image-gran-espresso.png',
+    img: '/images/headquarters/illustration-australia.svg',
+  },
+];
