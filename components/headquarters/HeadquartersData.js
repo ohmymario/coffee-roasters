e97d5ae7b205274d74d64c58818ba4c651624adb
Mario <mariob360@gmail.com>
2021-04-13 15:11:03 -0700

    Update headquartersdata file add in width and height for svg

--- components/headquarters/HeadquartersData.js
+++ components/headquarters/HeadquartersData.js
@@ -3,22 +3,30 @@ export const headquartersData = [
     country: 'United Kingdom',
     phone: '+44 1241 918425',
     address: `68 Asfordby Rd
-    Alcaston
-    SY61YA`,
+Alcaston
+SY6 1YA`,
+    width: 42,
+    height: 50,
     img: '/images/headquarters/illustration-uk.svg',
   },
   {
     country: 'Canada',
     phone: '+1 416 485 2997',
     address: `1528 Eglinton Avenue
-    Toronto
-    Ontario M4P1A6`,
+Toronto
+Ontario M4P 1A6`,
+    width: 52,
+    height: 50,
     img: '/images/headquarters/illustration-canada.svg',
   },
   {
     country: 'Australia',
     phone: '+61 4 9928 3629',
-    address: '/images/coffee/image-gran-espresso.png',
+    address: `36 Swanston Street
+Kewell
+Victoria`,
+    width: 49,
+    height: 44,
     img: '/images/headquarters/illustration-australia.svg',
   },
 ];
