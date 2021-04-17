e4a2feac33e1feb2634622c549400aaa38c7b5ac
Mario <mariob360@gmail.com>
2021-04-16 21:49:19 -0700

    populate all questions with prop data

--- components/question/Question.js
+++ components/question/Question.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
-const Question = () => (
-  <>
-    <div>
-      <h2>how</h2>
-      <span>arrow</span>
-    </div>
-    <div>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-    </div>
-  </>
-);
+const Question = props => {
+  console.log(props);
+  const { name, open, question, selected, selectedAnswer, selections } = props;
+  return (
+    <>
+      <div>
+        <h2>{question}</h2>
+        <span>arrow</span>
+      </div>
+      <div>
+        {selections.map(answer => (
+          <>
+            <h4>{answer.title}</h4>
+            <p>{answer.description}</p>
+          </>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Question;
