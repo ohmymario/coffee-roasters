643ada6327d547db9d4928c90ac9a23ef320bb58
Mario <mariob360@gmail.com>
2021-04-21 18:04:46 -0700

    add cursor to the selection help indicate that its interactive

--- components/selection/SelectionStyles.js
+++ components/selection/SelectionStyles.js
@@ -5,8 +5,9 @@ const SelectionStyles = styled.div`
   min-width: 255px;
   padding: 1.5em 0 1.5em 0;
   opacity: ${props => props.selected && '40%'};
-  border-bottom: 1px solid var(--greyOpacity)};
-  :last-child{
+  border-bottom: 1px solid var(--greyOpacity);
+  cursor: pointer;
+  :last-child {
     border-bottom: none;
   }
   :hover {
@@ -20,7 +21,6 @@ const SelectionStyles = styled.div`
   .selection-name {
     color: var(--darkGreyBlue);
   }
-
 `;
 
 export default SelectionStyles;
