36e1dcf7c54381111644dd0593bc70bf0aba1282
Mario <mariob360@gmail.com>
2021-05-04 11:36:23 -0700

    Add desktop styles to OrderModal component

--- components/orderModal/OrderModalStyles.js
+++ components/orderModal/OrderModalStyles.js
@@ -1,6 +1,42 @@
 import styled from 'styled-components';
 
-const OrderModalStyles = styled.div``;
+const OrderModalStyles = styled.div`
+  position: relative;
+  background: white;
+  max-width: 540px;
+  margin: auto;
+  border-radius: 10px;
+  overflow: hidden;
+
+  .title-bg {
+    background: var(--darkGreyBlue);
+    .title {
+      padding: 48px 56px 40px 56px;
+      h2 {
+        color: #ffffff;
+      }
+    }
+  }
+
+  .order {
+    padding: 56px;
+    &-summary {
+      margin-bottom: 47px;
+      h4 {
+        margin-bottom: 7px;
+        span {
+          color: var(--darkCyan);
+        }
+      }
+    }
+    &-checkout {
+      display: flex;
+      align-items: center;
+      gap: 15px;
+    }
+  }
+`;
+
 const Overlay = styled.div`
   display: flex;
   flex-direction: column;
@@ -13,4 +49,5 @@ const Overlay = styled.div`
   background: rgba(0, 0, 0, 0.5);
   z-index: 10000;
 `;
+
 export { OrderModalStyles, Overlay };
