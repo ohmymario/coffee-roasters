8f57da5273a08cf9351a0c3a3206fbc514c05ed7
Mario <mariob360@gmail.com>
2021-05-10 11:40:36 -0700

    add tablet styles for pagelink

--- components/pageLinks/PageLinksStyles.js
+++ components/pageLinks/PageLinksStyles.js
@@ -2,6 +2,11 @@ import styled from 'styled-components';
 
 const PageLinksStyles = styled.ul`
   display: flex;
+  padding: 0;
+  margin-bottom: 65px;
+  a:hover {
+    color: var(--lightCreamBG);
+  }
   li {
     font-family: 'Barlow', sans-serif;
     font-weight: 700;
