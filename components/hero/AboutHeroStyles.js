7ed998aa47bbf3e6371be2295e483e22aabc9bca
Mario <mariob360@gmail.com>
2021-05-11 14:20:10 -0700

    Add tablet styles to abouthero component

--- components/hero/AboutHeroStyles.js
+++ components/hero/AboutHeroStyles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import HeroBaseStyles from '../styles/HeroBaseStyles';
 
+// 689x400
+
 const AboutHeroStyles = styled(HeroBaseStyles)`
   height: 450px;
   margin-bottom: 10.5rem;
@@ -10,6 +12,24 @@ const AboutHeroStyles = styled(HeroBaseStyles)`
 
     h2 {
       margin-bottom: 0.6em;
+    }
+  }
+
+  @media (max-width: 768px) {
+    height: 400px;
+    margin-bottom: 9rem;
+
+    .hero-text {
+      width: 66%;
+      padding-left: 58px;
+
+      h2 {
+        margin-bottom: 0.75em;
+        font-size: 32px;
+      }
+      p {
+        max-width: 100%;
+      }
     }
   }
 `;
