5b11bfcb16c341672a3d393d794b53390619b58f
Mario <mariob360@gmail.com>
2021-05-17 13:13:58 -0700

    Add tablet styles to ordersummary component

--- components/orderSummary/OrderSummaryStyles.js
+++ components/orderSummary/OrderSummaryStyles.js
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 
+// 689 x 208
+
 const OrderSummaryStyles = styled.div`
   width: 100%;
-  padding: 47px 64px 47px 64px;
   background: #2c343e;
   border-radius: 10px;
   color: #ffffff;
   margin-bottom: 2.2em;
+  padding: 47px 64px 47px 64px;
 
   .order-title {
     opacity: 0.537;
@@ -19,6 +21,16 @@ const OrderSummaryStyles = styled.div`
     span {
       color: var(--darkCyan);
     }
+  }
+
+  @media (max-width: 768px) {
+    padding: 27px 44px 27px 44px;
+
+    .order-title {
+      font-size: 16px;
+    }
+
+    margin-bottom: 2.5em;
   }
 `;
 
